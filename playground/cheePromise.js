fead3292c6ba3b5be85beb5d98d26bbf018b1740
DmitryAlCh <dmi.al.ch@gmail.com>
2017-02-24 17:46:59 +0200

    technical commit, no features added

--- playground/cheePromise.js
+++ playground/cheePromise.js
@@ -14,7 +14,7 @@ var contactName = '.info > div > b';
 var contactPosition = '.info > div';
 
 
-var tempArr = [
+var tagArr = [
   companyFinder,
   titleFinder,
   descriptionFinder,
@@ -22,17 +22,32 @@ var tempArr = [
   contactName,
   contactPosition,
 ];
-var classToSearch = [tempArr[5]];
-
+var returnPropArr = [
+  'companyName',
+  'productTitle',
+  'productDescription',
+  'keywords',
+  'contactName',
+  'contactPhone',
+]
+var classToSearch = {
+  itemToSearch: tagArr,
+  propToReturn: returnPropArr
+};
+console.log(classToSearch);
 var getElementArray = (htmlPage, classToSearch) => {
 
   var returnedArray=[];
   // seach values for all provided classes
+  console.log(typeof classToSearch.itemToSearch);
+  classToSearch.itemToSearch.forEach(function (i, element){
+    console.log("searched tag: "+element);
+  });
   return new Promise((resolve, reject) => {
     //classToSearch must be an Array
-    if (classToSearch.isArray || classToSearch.length<1){
-      reject('Must provide an array with a list of classes to search');
-    }
+    // if (classToSearch.isArray || classToSearch.length<1){
+    //   reject('Must provide an array with a list of classes to search');
+    // }
       let $ = cheerio.load(htmlPage);
 
       // classToSearch.forEach(function(singleClassToSearch){
@@ -48,14 +63,18 @@ var getElementArray = (htmlPage, classToSearch) => {
         provider:"",
         description:""
       }
-      $(classToSearch[0]).each( function (i, element) {
+      var returnObj = {
+
+      };
+      $(classToSearch.itemToSearch).each( function (i, element) {
 
 
+          console.log(returnObj);
           // console.log("title: "+$(element).find('.item-head').text());
           // console.log("current DIVs class: "+ $(element).attr('class'));
           // console.log('Chilren count: '+$(element).children().length);
-          console.log('Elements that correspond to: '+classToSearch+": "+$(element).text());
-          returnedArray.push('Elements that correspond to: '+classToSearch+": "+$(element).text());
+          console.log('Elements that correspond to: '+classToSearch.itemToSearch+": "+$(element).text());
+          returnObj[classToSearch.propToReturn] = $(element).text();
           // if ($(element).find('.item-head').text()){
           //   jsonData.title = $(element).find('.item-head').text();
           //   return;
@@ -75,17 +94,17 @@ var getElementArray = (htmlPage, classToSearch) => {
       // console.log("*********** Total JSONdata: ******** ");
       // console.log(jsonData);
 
-      if (returnedArray.length>0) {
-        resolve(returnedArray);
+      if (returnObj) {
+        resolve(returnObj);
       }
         reject('No items found');
       });
 }
-// getElementArray(htmlPage,classToSearch).then((success)=>{
-//   console.log(success);
-// }, (err) =>{
-//   console.log("Something went wrong: "+ err);
-// });
+getElementArray(htmlPage,classToSearch).then((success)=>{
+  console.log(success);
+}, (err) =>{
+  console.log("Something went wrong: "+ err);
+});
 
 module.exports = {
   getElementArray
