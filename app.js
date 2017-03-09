c6882c106eb4a6c51b6a61bce28e3646f804f101
ArturKarbone <arturk@Arturs-MacBook-Pro.local>
2017-03-09 15:58:38 +0200

    Refactoring: splitting into function

--- app.js
+++ app.js
@@ -14,45 +14,57 @@ var goo = "http://google.com";
 var imdb = "http://www.imdb.com/title/tt1229340/"
 
 
-var getPage = request(page, function (error, response, html){
+var getPage = request(page, function (error, response, html) {
 
-  if (!error && response.statusCode == 200){
-    // here we parse all data
+  validResponse = (err, response) => {
+    return !error && response.statusCode == 200;
+  }
 
-      chee.getElementArray(html, whatToSearch).then(
-        // Got an obejct as a result
-        (result)=>{
-          console.log('***** Found results: (before encoding)******');
-          console.log(result);
-          // Encode object props to UTF8
-          encode.encode(result).then(
-            (encodedObj) => {
-              // convert JSON to CSV
-              exportCsv.convertJson(encodedObj).then(
-                (csv) => {
-                  // write to fileSystem ot database
-                  fs.writeFileSync('company.csv', csv, 'binary');
-                },
-                (fail) => {
-                  console.log('Unable to covert JSON to CSV');
-                  return;
-                }
-              ).catch((e)=>{
-                // when excel is open
-                console.log("FS writeFileSync somehow failed");
-              });
-            },
-            (fail) =>{
-              console.log(fail);
-              return;
-            }
-          )
-
+  //move this concern to a separate file
+  saveToFile = (saveRequest) => {
+    encode.encode(saveRequest.company).then(
+      (encodedObj) => {
+        // convert JSON to CSV
+        exportCsv.convertJson(encodedObj).then(
+          (csv) => {
+            // write to fileSystem ot database
+            fs.writeFileSync(saveRequest.fileName, csv, 'binary');
           },
-        (fail)=>{
-          console.log("***** Something went wrong: ******");
-          console.log(fail);
-          return;
+          (fail) => {
+            console.log('Unable to covert JSON to CSV');
+            return;
+          }
+        ).catch((e) => {
+          // when excel is open
+          console.log("FS writeFileSync somehow failed");
         });
-    }
-  });
+      },
+      (fail) => {
+        console.log(fail);
+        return;
+      }
+    )
+  }
+
+  if (validResponse(error, response)) {
+    //TODO: rename this to company parser or something like that   
+    chee.
+      getElementArray(html, whatToSearch)
+      .then((company) => {
+        
+        console.log('***** Found results: (before encoding)******');
+        console.log(company);
+        
+        saveToFile({
+          company: company,
+          fileName: 'company.csv'
+        });
+
+      },
+      (fail) => {
+        console.log("***** Something went wrong: ******");
+        console.log(fail);
+        return;
+      });
+  }
+});
